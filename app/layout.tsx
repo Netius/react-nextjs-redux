adf287e868b820cfc201bbdf9cf7468adbb3cebc
Fabio <fabio@axicon.no>
2024-08-08 10:19:31 +0200

    Added bootstrap loader to prj

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,22 @@ import Image from "next/image";
 import type { ReactNode } from "react";
 import { StoreProvider } from "./StoreProvider";
 import { Nav } from "./components/Nav";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import "./styles/globals.css";
 import styles from "./styles/layout.module.css";
+
+import BootstrapProvider from "../lib/features/bootstrap/loadBootstrap"
 
 interface Props {
   readonly children: ReactNode;
 }
 
+
+
 export default function RootLayout({ children }: Props) {
+  
   return (
     <StoreProvider>
+      <BootstrapProvider />
       <html lang="en">
         <body>
           <section className={styles.container}>
