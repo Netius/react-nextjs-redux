529fb7c98607114544229d5df4bbc3527d19614d
Fabio <fabio@axicon.no>
2024-08-09 12:46:49 +0200

    Fixed type and reducers

--- app/data/toastMessageSlice.tsx
+++ app/data/toastMessageSlice.tsx
@@ -1,23 +1,28 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 let toastMsg: string = "";
+let css: string = "";
 
 // TODO Need to implement logic based here
-export const toastMessageSlice = createSlice({  
+export const toastMessageSlice = createSlice({
   name: 'toast',
   initialState: {
     toastMessage: toastMsg,
+    cssClass: css
   },
   reducers: {
     successToast: (state, action: PayloadAction<string>) => {
-        state.toastMessage = action.payload;
+      state.toastMessage = action.payload[0];
+      state.cssClass = action.payload[1];
     },
-    // removeFromCart: (state, action: PayloadAction<number>) => {
-    //   const indexOfId = state.cartProductIds.indexOf(action.payload)
-    //   state.cartProductIds.splice(indexOfId, 1); 
-    // },
-    // clearAllItems: (state,action: PayloadAction<number> ) => {
-    //   state.cartProductIds = [];
-    // }
+    removeToast: (state, action: PayloadAction<string>) => {
+      state.toastMessage = action.payload[0];
+      state.cssClass = action.payload[1];
+    },
+    clearToast: (state, action: PayloadAction<string>) => {
+      state.toastMessage = "";
+      state.cssClass = "";
+
+    }
   }
 })
